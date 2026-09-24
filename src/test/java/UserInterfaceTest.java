03832567271692f5c42a3f9f1159ed83c33cfaa9
scrap-devs <lucasung07@gmail.com>
2026-09-24 01:43:58 -0400

    added UserInterface.java + UserInterfaceTest.java + Documentation

---
+++ src/test/java/UserInterfaceTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserInterfaceTest {
+
+    @Test
+    void userInterfaceCanBeCreated() {
+        UserInterface ui = new UserInterface(new ProgramControl());
+
+        assertNotNull(ui);
+    }
+
+    @Test
+    void noArgumentsDoesNotCrash() {
+        UserInterface ui = new UserInterface(new ProgramControl());
+
+        assertDoesNotThrow(() -> ui.run(new String[]{}));
+    }
+
+    @Test
+    void invalidFileNumberDoesNotCrash() {
+        UserInterface ui = new UserInterface(new ProgramControl());
+
+        assertDoesNotThrow(() -> ui.run(new String[]{"abc"}));
+    }
+}
