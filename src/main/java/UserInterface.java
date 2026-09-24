001e0954ff9bdb5d0d7d4340f5eacccf20df544e
scrap-devs <lucasung07@gmail.com>
2026-09-24 01:44:44 -0400

    removed methods/added commented code

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -8,37 +8,37 @@ public class UserInterface {
         this.programControl = programControl;
     }
 
-    public void run(String[] args) {
-        if (args.length == 0) {
-            displayFileList();
-        } else {
-            displayFileContents(args);
-        }
-    }
+//    public void run(String[] args) {
+//        if (args.length == 0) {
+//            displayFileList();
+//        } else {
+//            displayFileContents(args);
+//        }
+//    }
 
-    private void displayFileList() {
-        List<String> files = programControl.getFileList();
-        for (int i = 0; i < files.size(); i++) {
-            System.out.printf("%02d %s%n", i + 1, files.get(i));
-        }
-    }
+//    private void displayFileList() {
+//        List<String> files = programControl.getFileList();
+//        for (int i = 0; i < files.size(); i++) {
+//            System.out.printf("%02d %s%n", i + 1, files.get(i));
+//        }
+//    }
 
-    private void displayFileContents(String[] args) {
-        int fileNumber;
-        try {
-            fileNumber = Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
-            System.out.println("Error: '" + args[0] + "' is not a valid file number.");
-            return;
-        }
-
-        String keyFilePath = args.length > 1 ? args[1] : null;
-
-        try {
-            String contents = programControl.getFileContents(fileNumber, keyFilePath);
-            System.out.println(contents);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
+//    private void displayFileContents(String[] args) {
+//        int fileNumber;
+//        try {
+//            fileNumber = Integer.parseInt(args[0]);
+//        } catch (NumberFormatException e) {
+//            System.out.println("Error: '" + args[0] + "' is not a valid file number.");
+//            return;
+//        }
+//
+//        String keyFilePath = args.length > 1 ? args[1] : null;
+//
+//        try {
+//            String contents = programControl.getFileContents(fileNumber, keyFilePath);
+//            System.out.println(contents);
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Error: " + e.getMessage());
+//        }
+//    }
 }
