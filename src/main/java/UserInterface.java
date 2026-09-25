41909b77f49eae69a608e94ad66e00df79becd9b
scrap-devs <lucasung07@gmail.com>
2026-09-25 14:57:55 -0400

    added uncommented code + updated userinterface documentation + fixed tests using try/catch

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -8,37 +8,50 @@ public class UserInterface {
         this.programControl = programControl;
     }
 
-//    public void run(String[] args) {
-//        if (args.length == 0) {
-//            displayFileList();
-//        } else {
-//            displayFileContents(args);
-//        }
-//    }
+    public void run(String[] array) {
+        if (array.length == 0) {
+            displayFileList();
+        } else {
+            displayFileContents(array);
+        }
+    }
 
-//    private void displayFileList() {
-//        List<String> files = programControl.getFileList();
-//        for (int i = 0; i < files.size(); i++) {
-//            System.out.printf("%02d %s%n", i + 1, files.get(i));
-//        }
-//    }
+    private void displayFileList() {
+        List<String> files = programControl.getAvailableFiles();
+        for (int i = 0; i < files.size(); i++) {
+            String number = (i + 1) < 10 ? "0" + (i + 1) : "" + (i + 1);
+            System.out.println(number + " " + files.get(i));
+        }
+    }
 
-//    private void displayFileContents(String[] args) {
-//        int fileNumber;
-//        try {
-//            fileNumber = Integer.parseInt(args[0]);
-//        } catch (NumberFormatException e) {
-//            System.out.println("Error: '" + args[0] + "' is not a valid file number.");
-//            return;
-//        }
-//
-//        String keyFilePath = args.length > 1 ? args[1] : null;
-//
-//        try {
-//            String contents = programControl.getFileContents(fileNumber, keyFilePath);
-//            System.out.println(contents);
-//        } catch (IllegalArgumentException e) {
-//            System.out.println("Error: " + e.getMessage());
-//        }
-//    }
+    private void displayFileContents(String[] array) {
+        int fileNumber;
+        try {
+            fileNumber = Integer.parseInt(array[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: '" + array[0] + "' is not a valid file number.");
+            return;
+        }
+
+        List<String> files = programControl.getAvailableFiles();
+        if (fileNumber < 1 || fileNumber > files.size()) {
+            System.out.println("Error: No file found matching number " + fileNumber + ".");
+            return;
+        }
+        String fileName = files.get(fileNumber - 1);
+
+        String contents;
+        if (array.length > 1) {
+            contents = programControl.decipherFile(fileName, array[1]);
+        } else {
+            contents = programControl.decipherFile(fileName);
+        }
+
+        if (contents == null) {
+            System.out.println("Error: Unable to read or decipher '" + fileName + "'.");
+            return;
+        }
+
+        System.out.println(contents);
+    }
 }
--- src/test/java/UserInterfaceTest.java
+++ src/test/java/UserInterfaceTest.java
@@ -5,23 +5,53 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UserInterfaceTest {
 
     @Test
-    void userInterfaceCanBeCreated() {
+    void canMakeUserInterface() {
+        // Check that we can create a UserInterface object
         UserInterface ui = new UserInterface(new ProgramControl());
-
         assertNotNull(ui);
     }
 
-//    @Test
-//    void noArgumentsDoesNotCrash() {
-//        UserInterface ui = new UserInterface(new ProgramControl());
-//
-//        assertDoesNotThrow(() -> ui.run(new String[]{}));
-//    }
-//
-//    @Test
-//    void invalidFileNumberDoesNotCrash() {
-//        UserInterface ui = new UserInterface(new ProgramControl());
-//
-//        assertDoesNotThrow(() -> ui.run(new String[]{"abc"}));
-//    }
+    @Test
+    void noInputDoesNotCrash() {
+        // Check that running with nothing typed in does not crash
+        UserInterface ui = new UserInterface(new ProgramControl());
+        try {
+            ui.run(new String[]{});
+        } catch (Exception e) {
+            fail("Program crashed with no input");
+        }
+    }
+
+    @Test
+    void lettersInsteadOfNumberDoesNotCrash() {
+        // Check that typing letters instead of a number does not crash
+        UserInterface ui = new UserInterface(new ProgramControl());
+        try {
+            ui.run(new String[]{"abc"});
+        } catch (Exception e) {
+            fail("Program crashed with letters instead of a number");
+        }
+    }
+
+    @Test
+    void fileNumberTooHighDoesNotCrash() {
+        // Check that asking for a file number that does not exist does not crash
+        UserInterface ui = new UserInterface(new ProgramControl());
+        try {
+            ui.run(new String[]{"999"});
+        } catch (Exception e) {
+            fail("Program crashed with a file number that does not exist");
+        }
+    }
+
+    @Test
+    void validFileNumberDoesNotCrash() {
+        // Check that asking for a file number that does exist does not crash
+        UserInterface ui = new UserInterface(new ProgramControl());
+        try {
+            ui.run(new String[]{"1"});
+        } catch (Exception e) {
+            fail("Program crashed with a valid file number");
+        }
+    }
 }
