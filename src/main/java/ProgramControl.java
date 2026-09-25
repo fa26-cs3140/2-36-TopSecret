91a58f7e7284c06eda86d070a2eb2a5017881757
ltfk12 <toufiklina12@gmail.com>
2026-09-25 14:06:31 -0400

    Add ProgramControl and FileHandler integration tests

--- src/main/java/ProgramControl.java
+++ src/main/java/ProgramControl.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class ProgramControl {
 
     private final FileHandler fileHandler;
@@ -6,7 +8,43 @@ public class ProgramControl {
         fileHandler = new FileHandler();
     }
 
+    // Get the list of mission files from Member C's FileHandler
+    public List<String> getAvailableFiles() {
+        return fileHandler.getAvailableFiles();
+    }
+
+    // Get a file without deciphering it
     public String getFileContents(String fileName) {
-        return fileHandler.readFile(fileName);
+        return fileHandler.readDataFile(fileName);
+    }
+
+    // Get and decipher a file using the default key
+    public String decipherFile(String fileName) {
+        String contents = fileHandler.readDataFile(fileName);
+
+        if (contents == null) {
+            return null;
+        }
+
+        String key = fileHandler.readCipherFile("key.txt");
+
+        if (key == null) {
+            return null;
+        }
+
+        Cipher cipher = new Cipher(key);
+        return cipher.decipher(contents);
+    }
+
+    // Get and decipher a file using an alternate key
+    public String decipherFile(String fileName, String key) {
+        String contents = fileHandler.readDataFile(fileName);
+
+        if (contents == null) {
+            return null;
+        }
+
+        Cipher cipher = new Cipher(key);
+        return cipher.decipher(contents);
     }
 }
--- src/test/java/ProgramControlTest.java
+++ src/test/java/ProgramControlTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ProgramControlTest {
@@ -16,6 +18,35 @@ public class ProgramControlTest {
         ProgramControl control = new ProgramControl();
 
         String result = control.getFileContents("doesNotExist.txt");
+
+        assertNull(result);
+    }
+
+    @Test
+    void availableFilesCanBeRequested() {
+        ProgramControl control = new ProgramControl();
+
+        List<String> files = control.getAvailableFiles();
+
+        assertNotNull(files);
+    }
+
+    @Test
+    void missingCipheredFileReturnsNull() {
+        ProgramControl control = new ProgramControl();
+
+        String result = control.decipherFile("doesNotExist.cip");
+
+        assertNull(result);
+    }
+
+    @Test
+    void alternateKeyCanBeUsed() {
+        ProgramControl control = new ProgramControl();
+
+        String key = "abc\nxyz";
+
+        String result = control.decipherFile("doesNotExist.cip", key);
 
         assertNull(result);
     }
